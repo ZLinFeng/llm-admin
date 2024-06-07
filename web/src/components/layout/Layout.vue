39005c233c10837e27b36bbbe7dbd3891e632985
ZLinFeng <you@example.com>
2024-06-07 16:31:02 +0800

    dock

--- web/src/components/layout/Layout.vue
+++ web/src/components/layout/Layout.vue
@@ -4,14 +4,9 @@ import SplitterPanel from "primevue/splitterpanel"
 </script>
 
 <template>
-  <Splitter class="app-container" :step="10" :gutter-size="0">
-    <SplitterPanel :size="12" :min-size="12">Sider</SplitterPanel>
-    <SplitterPanel :size="88" :min-size="88">
-      <Splitter layout="vertical" :gutter-size="0">
-        <SplitterPanel :size="8" :min-size="8">Header</SplitterPanel>
-        <SplitterPanel :size="92" :min-size="92">Main</SplitterPanel>
-      </Splitter>
-    </SplitterPanel>
+  <Splitter class="app-container" :gutter-size="0" layout="vertical">
+    <SplitterPanel :size="8" :min-size="8">Header</SplitterPanel>
+    <SplitterPanel :size="92" :min-size="92">Main</SplitterPanel>
   </Splitter>
 </template>
 
---
+++ web/src/components/layout/LayoutHeader.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="app-header">
+    <div class="app-logo">
+
+    </div>
+    <div class="wel-wrapper">
+      <div class="dock-btn">
+
+      </div>
+      <div class="wel">
+
+      </div>
+      <div class="person-menu">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.app-header {
+    display: flex;
+}
+</style>
